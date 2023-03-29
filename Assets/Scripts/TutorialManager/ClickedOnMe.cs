9bd6dbae78b6c60be5a8c3f6c32301594c2d6821
Llwnr <noregrets.llwnr@gmail.com>
2023-03-29 13:54:44 +0545

    tutorial

---
+++ Assets/Scripts/TutorialManager/ClickedOnMe.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickedOnMe : MonoBehaviour
+{
+    private bool hasBeenClicked = false;
+
+    public bool HasBeenClicked(){
+        return hasBeenClicked;
+    }
+
+    private void OnMouseDown() {
+        WhenClicked();
+    }
+    public void WhenClicked() {
+        if(hasBeenClicked) return;
+        //Let player go to next step of tutorial where they need to click on next area
+        hasBeenClicked = true;
+        transform.parent.GetComponent<ManageClickingOrder>().GoToNextArea();
+    }
+}
---
+++ Assets/Scripts/TutorialManager/ManageClickingOrder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageClickingOrder : MonoBehaviour
+{
+    [SerializeField]private List<GameObject> clickAreas = new List<GameObject>();
+    private int index = 0;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Only activate one clickable area at a time
+        for(int i=0; i<clickAreas.Count; i++){
+            if(i == index) continue;
+
+            clickAreas[i].SetActive(false);
+        }
+    }
+
+    public void GoToNextArea(){
+        Debug.Log("Move to next area");
+        index++;
+        if(index >= clickAreas.Count) index = clickAreas.Count-1;
+        //Activate next click area
+        clickAreas[index].SetActive(true);
+    }
+}
