d213fc9c0ace059c202f7fa94bd91532da9a856d
Llwnr <noregrets.llwnr@gmail.com>
2023-04-11 16:34:33 +0545

    tutUpdate

---
+++ Assets/Scripts/TutorialManager/ClickAnywhere.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickAnywhere : MonoBehaviour
+{
+    [SerializeField]private bool hasBeenClicked = false;
+
+    public bool HasBeenClicked(){
+        return hasBeenClicked;
+    }
+
+    private void Update() {
+        if(Input.GetMouseButtonDown(0)){
+            WhenClicked();
+        }
+    }
+
+    public void WhenClicked() {
+        if(hasBeenClicked) return;
+        //Let player go to next step of tutorial where they need to click on next area
+        hasBeenClicked = true;
+        transform.parent.GetComponent<ManageClickingOrder>().GoToNextArea();
+    }
+}
